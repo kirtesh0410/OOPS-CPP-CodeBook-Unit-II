8b1bd6c53d18c1fbc9965324d657ad0927aa30d7
Kirtesh Bharambe <kirteshbharambe0410@gmail.com>
2026-09-23 04:01:08 +0530

    Create Program11.cpp

---
+++ Program11.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Shape {
+public:
+    virtual double area() const = 0;
+    virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape {
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double givenLength, double givenWidth)
+        : length(givenLength), width(givenWidth) {}
+
+    double area() const override {
+        return length * width;
+    }
+};
+
+class Circle : public Shape {
+private:
+    double radius;
+
+public:
+    explicit Circle(double givenRadius)
+        : radius(givenRadius) {}
+
+    double area() const override {
+        return 3.141592653589793 * radius * radius;
+    }
+};
+
+int main() {
+    Rectangle rectangle(6.0, 4.0);
+    Circle circle(3.0);
+
+    std::cout << "Rectangle Area: " << rectangle.area() << '\n';
+    std::cout << "Circle Area: " << circle.area() << '\n';
+
+    return 0;
+}
